599c2d60eb53571b16d6650bb97ca46cecc6144d
MatthewChT <m.tait2@newcastle.ac.uk>
2021-10-28 14:43:46 +0100

    allowed inputted contacts to be added to a text file

--- contacts.py
+++ contacts.py
@@ -13,18 +13,26 @@ class Contact:
 #main
 if __name__=="__main__":
 
-    AddContact = input('Do you want to add a new input? (Y/N)')
+    AddContact = input('Do you want to add a new input? (Y/N):')
     while AddContact == ('Y'):
 
         #create a contact through accepting inputs
         con1 = Contact(input("Enter name: "), input("Enter date of birth: "), input("Enter address: "), input("Enter phone number: "))
 
         #prints the contents of contact 1
-        print('Name: ' + con1.name,
-            '\nDate of Birth: ' + con1.dob,
-            '\nAddress: ' + con1.address,
-            '\nPhone Number: ' + con1.phone_num)
+        print('\nName: ' + con1.name,
+                '  Date of Birth: ' + con1.dob,
+                '  Address: ' + con1.address,
+                '  Phone Number: ' + con1.phone_num)
 
+        contactp1 = ('Name: ' +con1.name, '  Date of Birth: ' + con1.dob)
+        contactp2 = ('  Address: ' + con1.address, '  Phone Number: ' + con1.phone_num)
+        contact1 = str(contactp1 + contactp2)
+
+
+        file = open('Contacts.txt', 'a')
+        file.write('\n' + contact1)
+        file.close()
         AddContact = input('Do you want to add a new input? (Y/N)')
 
     else:
