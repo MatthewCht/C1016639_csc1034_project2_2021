30706ec9d82646101adcd72694572d1588897571
MatthewChT <m.tait2@newcastle.ac.uk>
2021-10-28 14:51:15 +0100

    renamed con1 to con to be more appropriate and added comments

--- contacts.py
+++ contacts.py
@@ -13,23 +13,29 @@ class Contact:
 #main
 if __name__=="__main__":
 
+    #starts a while loop so until the user enters something other than 'Y' it keeps adding new contacts
     AddContact = input('Do you want to add a new input? (Y/N):')
     while AddContact == ('Y'):
 
         #create a contact through accepting inputs
-        con1 = Contact(input("Enter name: "), input("Enter date of birth: "), input("Enter address: "), input("Enter phone number: "))
+        con = Contact(input("Enter name: "),
+                      input("Enter date of birth: "),
+                      input("Enter address: "),
+                      input("Enter phone number: "))
 
-        #prints the contents of contact 1
-        print('\nName: ' + con1.name,
-                '  Date of Birth: ' + con1.dob,
-                '  Address: ' + con1.address,
-                '  Phone Number: ' + con1.phone_num)
+        #prints the contents of the contact
+        print('\nName: ' + con.name,
+                '  Date of Birth: ' + con.dob,
+                '  Address: ' + con.address,
+                '  Phone Number: ' + con.phone_num)
 
-        contactp1 = ('Name: ' +con1.name, '  Date of Birth: ' + con1.dob)
-        contactp2 = ('  Address: ' + con1.address, '  Phone Number: ' + con1.phone_num)
+        #takes the first and second half of the contact then concatonates them so it can be written to the file
+        contactp1 = ('Name: ' +con.name, '  Date of Birth: ' + con.dob)
+        contactp2 = ('  Address: ' + con.address, '  Phone Number: ' + con.phone_num)
         contact1 = str(contactp1 + contactp2)
 
 
+        #writes the contact to the text file
         file = open('Contacts.txt', 'a')
         file.write('\n' + contact1)
         file.close()
