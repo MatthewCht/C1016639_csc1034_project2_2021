2877f0eb81589b2d4d79523e1e184eda35343e9f
MatthewChT <m.tait2@newcastle.ac.uk>
2021-11-16 10:26:49 +0000

    allows the entire list to be viewed and renamed variables and edited layout to improve syntax and readabilty

--- contacts.py
+++ contacts.py
@@ -1,7 +1,7 @@
-#class definition
+# class definition
 class Contact:
 
-    #constructor
+    # constructor
     def __init__(self, name, dob, address, phone_num):
         self.name = name
         self.dob = dob
@@ -9,70 +9,77 @@ class Contact:
         self.phone_num = phone_num
 
 
+# main
+if __name__ == "__main__":
 
-#main
-if __name__=="__main__":
+    # shows the entire contact list
+    ContactList = input('Do you want view the contacts list? (Y/N):')
+    if ContactList == 'Y':
+        file = open('Contacts.txt')
+        lines = file.readlines()
+        for line in lines:
+            print(line)
+        file.close()
 
-    #starts a while loop so until the user enters something other than 'Y' it keeps adding new contacts
+    # starts a while loop so until the user enters something other than 'Y' it keeps adding new contacts
     AddContact = input('Do you want to add a new input? (Y/N):')
-    while AddContact == ('Y'):
+    while AddContact == 'Y':
 
-        #create a contact through accepting inputs
+        # create a contact through accepting inputs
         con = Contact(input("Enter name: "),
                       input("Enter date of birth: "),
                       input("Enter address: "),
                       input("Enter phone number: "))
 
-        #prints the contents of the contact
+        # prints the contents of the contact
         print('\nName: ' + con.name,
-                '  Date of Birth: ' + con.dob,
-                '  Address: ' + con.address,
-                '  Phone Number: ' + con.phone_num)
+              '  Date of Birth: ' + con.dob,
+              '  Address: ' + con.address,
+              '  Phone Number: ' + con.phone_num)
 
-        #takes the first and second half of the contact then concatonates them so it can be written to the file
-        contactp1 = ('Name: ' +con.name, '  Date of Birth: ' + con.dob)
-        contactp2 = ('  Address: ' + con.address, '  Phone Number: ' + con.phone_num)
-        contact1 = str(contactp1 + contactp2)
+        # takes the first and second half of the contact then concatenates them so it can be written to the file
+        ContactP1 = ('Name: ' + con.name, '  Date of Birth: ' + con.dob)
+        ContactP2 = ('  Address: ' + con.address, '  Phone Number: ' + con.phone_num)
+        Contact1 = str(ContactP1 + ContactP2)
 
-
-        #writes the contact to the text file
+        # writes the contact to the text file
         file = open('Contacts.txt', 'a')
-        file.write('\n' + contact1)
+        file.write('\n' + Contact1)
         file.close()
         AddContact = input('Do you want to add a new input? (Y/N)')
 
-    #asks user for input to feed into while loop
+    # asks user for input to feed into while loop
     else:
-        findcontact = input('Do you want to search for a contact? (Y/N)')
+        FindContact = input('Do you want to search for a contact? (Y/N)')
 
-    #creates a while loop for finding a contact
-    while findcontact == ('Y'):
+    # creates a while loop for finding a contact
+    while FindContact == 'Y':
 
-        contactkey = input('What is the Contacts name (case sensitive):')
-        contactfile = open("Contacts.txt")
+        ContactKey = input('What is the Contacts name (case sensitive):')
+        ContactFile = open("Contacts.txt")
 
         # reference from 'https://www.kite.com/python/answers/how-to-search-for-text-in-a-file-in-python'
-        for line in contactfile:
-            if contactkey in line:
+        for line in ContactFile:
+            if ContactKey in line:
                 print(line)
-        findcontact = input('Do you want to search for a contact? (Y/N)')
+        FindContact = input('Do you want to search for a contact? (Y/N)')
 
     else:
-        editcontact = input('Do you want to edit a contact? (Y/N)')
+        EditContact = input('Do you want to edit a contact? (Y/N)')
 
-    while editcontact == ('Y'):
-        contactfile = open("Contacts.txt")
-        contactkey = input('What is the Contacts name (case sensitive):')
-        for line in contactfile:
-            if contactkey in line:
-                contactkey = line
+    while EditContact == 'Y':
+        ContactFile = open("Contacts.txt")
+        ContactKey = input('What is the Contacts name (case sensitive):')
+        for line in ContactFile:
+            if ContactKey in line:
+                ContactKey = line
 
         with open('Contacts.txt', 'r') as file:
             lines = file.readlines()
 
         with open('Contacts.txt', 'w') as file:
             for line in lines:
-                if line.strip('\n') != contactkey:
+                if line.strip('\n') != ContactKey:
                     file.write(line)
 
         con = Contact(input("Enter name: "),
@@ -86,23 +93,17 @@ if __name__=="__main__":
               '  Address: ' + con.address,
               '  Phone Number: ' + con.phone_num)
 
-        # takes the first and second half of the contact then concatonates them so it can be written to the file
-        contactp1 = ('Name: ' + con.name, '  Date of Birth: ' + con.dob)
-        contactp2 = ('  Address: ' + con.address, '  Phone Number: ' + con.phone_num)
-        contact1 = str(contactp1 + contactp2)
+        # takes the first and second half of the contact then concatenates them so it can be written to the file
+        ContactP1 = ('Name: ' + con.name, '  Date of Birth: ' + con.dob)
+        ContactP2 = ('  Address: ' + con.address, '  Phone Number: ' + con.phone_num)
+        Contact1 = str(ContactP1 + ContactP2)
 
         # writes the contact to the text file
         file = open('Contacts.txt', 'a')
-        file.write('\n' + contact1)
+        file.write('\n' + Contact1)
         file.close()
 
-        editcontact = input('Do you want to edit a contact? (Y/N)')
+        EditContact = input('Do you want to edit a contact? (Y/N)')
 
 else:
     print('All operations complete')
-
-
-
-
-
-
